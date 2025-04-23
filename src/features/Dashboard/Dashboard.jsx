f203522bc766ada397d693d65fd7b8a49b6af023
Juan Esteban Londoño Uribe <jelu0204@gmail.com>
2025-04-22 20:06:20 -0500

    Update dashboard

--- src/features/Dashboard/Dashboard.jsx
+++ src/features/Dashboard/Dashboard.jsx
@@ -26,7 +26,7 @@ function Dashboard() {
 
     const navItems = [
         { to: '/', label: 'Inicio' },
-        { to: '', label: 'Usuarios' },
+        { to: '', label: 'Usuarios', show: role === 1 },
         { to: 'Roles', label: 'Roles', show: role === 1 },
         { to: 'Company', label: 'Compañía', show: role === 1 },
         { to: 'Products', label: 'Productos' },
